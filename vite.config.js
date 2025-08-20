8e7433000bc8f89c4d20840160c4ff7dd0e77b47
flash3105 <80648288+flash3105@users.noreply.github.com>
2025-08-20 12:35:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: './',  // ✅ use relative paths on Render
   resolve: {
     alias: {
